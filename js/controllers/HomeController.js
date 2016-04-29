d41570e4ce04a4a00bd1804bd2a2c810e4d2369a
np397 <nidhipatel062194@gmail.com>
2016-04-29 15:41:25 -0400

    Created title, owner and github variables inside the  HomeController

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -97,7 +97,9 @@ app.controller('HomeController', ['$scope', function($scope) {
 
 	
 	/* ADD VARIABLES FOR STEP 3 HERE */
-	
+	$scope.title = "IMBD + Nidhi's Top 8 Movies";
+	$scope.owner = "Nidhi";
+	$scope.github = "https://github.com/np397/is219s16patel-p3";
 	
 	
 	
